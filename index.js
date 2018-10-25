ce7651cd025ebdff87fe69b89bbb52d96dfae993
Andrea <andreamayhirji@gmail.com>
2018-10-25 01:28:03 +0000

    solved

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN){
+    return toN;
+  } return fromN + sum(fromN + 1, toN);
 }
+//So if we called the function with the following values: sum(3, 7), it would sum 3 + 4 + 5 + 6 + 7 and output 25.
+console.log(sum(5, 5));
 
 module.exports = sum;
